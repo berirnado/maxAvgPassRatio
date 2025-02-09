0703ae8e9aef3ad1103369dd00a3ebff546bf0c5
Bernardo <be.zito@hotmail.com>
2025-02-09 15:18:29 -0300

    Cria a funcao do leetcode e struct de classe

--- feitoEmCasa.c
+++ feitoEmCasa.c
@@ -1,6 +1,15 @@
 #include <stdio.h>;
 #include <stdlib.h>;
 
+typedef struct{
+    int pass;
+    int total;
+} Class;
+
+double maxAverageRatio(Class* classes, int classesSize, int extraStudents) {
+    
+}
+
 int main() {
     //TODO: Criar chamada para a função do leetcode
 }
